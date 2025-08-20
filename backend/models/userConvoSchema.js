a4bc5b4d7d6cbe3a4759305f66ed0431b0fa9e8c
Ethan <ethan.pascual@gmail.com>
2025-08-19 22:40:20 -0400

    create new user with starting story and state

--- backend/models/userConvoSchema.js
+++ backend/models/userConvoSchema.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
+const GameState = require('./stateSchema.js')
 
 const userConvo = mongoose.Schema({
     userId: String,
     convo: [{
         role: String,
-        content: String
+        content: String,
+        currGameState: { type: mongoose.Schema.Types.ObjectId, ref: 'GameState' }
     }]
 })
 
--- backend/storyCreationService.js
+++ backend/storyCreationService.js
@@ -2,7 +2,7 @@ const {OpenAI} = require("openai")
 
 async function generateStoryLine(userConvo, action){
     const openai = new OpenAI({apiKey: process.env.Open_AI_KEY})
-    const history = [{role: 'system', content: 'You are a dungeons and dragons master creating a fantasy world choose your own adventure story for the user based on the user actions. Please create the next section of the storyline based on the user\'s most recent action. Keep it a paragraph max and make sure it is relevant to the rest of the story.'}]
+    const history = [{role: 'system', content: 'You are a dungeons and dragons master creating a fantasy world choose your own adventure story for the user based on the user actions. Please create the next section of the storyline based on the user\'s most recent action. Try to guide the user towards a specific action, but don\'t outright tell them what to do. Keep it a paragraph max and make sure it is relevant to the rest of the story.'}]
     history.push(...userConvo)
     history.push({role: "user", content: action})
     const output = await openai.chat.completions.create({
