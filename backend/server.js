1c4b4d3302aaaf14c1cbd60fd955e2191f867a5b
Ethan <ethan.pascual@gmail.com>
2026-01-07 17:38:14 -0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,9 @@ const {connectDB, connectVectorDB} = require('./config.js')
 const gameRoutes = require('./gameRoutes.js')
 const cors = require('cors')
 
-const corsOptions = {
-    origin: "https://infiniquest.onrender.com/"
-}
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 
 connectDB();
